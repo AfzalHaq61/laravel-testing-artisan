7749bedfa78829e8f021c2dc552627b8a4633351
AfzalHaq61 <afzaldev6@gmail.com>
2023-09-12 16:54:44 +0500

    curd php unit testing

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
@@ -16,5 +18,48 @@ class UserTest extends TestCase
         $response = $this->get('/login');
 
         $response->assertStatus(200);
+    }
+
+    public function test_duplication(): void
+    {
+        $user1 = User::make([
+            'name' => 'Afzal',
+            'email' => 'afzal001@gmail.com'
+        ]);
+
+        $user2 = User::make([
+            'name' => 'Noor',
+            'email' => 'afzal002@gmail.com'
+        ]);
+
+        $this->assertTrue($user1->name != $user2->name);
+    }
+
+    public function test_delete_user() {
+
+        $user = User::factory()->count(1)->make();
+
+        $user = User::first();
+
+        if($user) {
+            $user->delete();
+        }
+
+        $this->assertTrue(true);
+
+    }
+
+    public function test_it_stores_new_users() {
+
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::HOME);
+
     }
 }
